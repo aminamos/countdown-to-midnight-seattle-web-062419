503bee06195abb4451a5861b792e16302e08f0cf
Amin Amos <aminamosahs@gmail.com>
2019-06-13 15:16:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,16 +2,16 @@
 
 def countdown(x)
   while x != 0 
-    x -= 1
     puts "#{x} SECOND(S)!"
+    x -= 1
   end
   puts "happy new year!".upcase
 end
 
 def countdown_with_sleep(x)
   while x != 0 
-    x -= 1
     puts "#{x} SECOND(S)!"
+    x -= 1
     sleep 1
   end
   puts "HAPPY NEW YEAR!"
