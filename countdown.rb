9632288db3964785a2d27f5fce5a9e97ea4f5195
Amin Amos <aminamosahs@gmail.com>
2019-06-13 15:13:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,15 @@
 #write your code here
 
 def countdown(x)
-  while x > 0 
+  while x != 0 
+    x -= 1
     puts "#{x} SECOND(S)!"
   end
 end
 
 def countdown_with_sleep(x)
-  while x > 0 
+  while x != 0 
+    x -= 1
     puts "#{x} SECOND(S)!"
     sleep 1
   end
